84704459f3f74772329f95b6d88964f7e6c41093
thomaslynn132 <132193278+thomaslynn132@users.noreply.github.com>
2023-11-09 18:50:17 +0630

    11.9.2023,18:49

--- src/Tenses.jsx
+++ src/Tenses.jsx
@@ -3,8 +3,9 @@ export default function Tenses() {
   return (
     <>
       <div className="container">
-        Verbs need tenses in order to have a meaningful sentence. There are 12
-        kinds of tenses.
+        Verbs need tenses in order to have a meaningful sentence. Tenses are one
+        of the most points to know in order to have a grammartically correct
+        sentences. There are 12 kinds of tenses.
         <table>
           <tr>
             <th>
@@ -62,7 +63,7 @@ export default function Tenses() {
           </tr>
         </table>
         <h3>
-          Conditions needed to be simple present tense <br />
+          Simple Present Tense <br />
           V1
         </h3>
         <ul typeof="dot">
@@ -93,7 +94,7 @@ export default function Tenses() {
           </li>
         </ul>
         <h3>
-          Conditions needed to be simple past tense
+          Simple Past Tense
           <br />
           V2
         </h3>
@@ -122,7 +123,7 @@ export default function Tenses() {
           </li>
         </ul>
         <h3>
-          Conditions needed to be simple future tense <br />
+          Simple Future Tense <br />
           (will + V1 or going to + V1)
         </h3>
         <ul typeof="dot">
@@ -150,7 +151,7 @@ export default function Tenses() {
           </li>
         </ul>
         <h3>
-          Conditions needed to be present perfect tense <br />
+          Present Perfect Tense <br />
           has/have + V3
         </h3>
         <ul typeof="dot">
@@ -160,11 +161,11 @@ export default function Tenses() {
             (Example. I have been doing these things for years.)
           </li>
           <li>
-            unfinished time words <br />
+            Unfinished time words <br />
             (Example. I haven't seen Mandy for a few weeks.)
           </li>
           <li>
-            present situations that happened recently <br />
+            Present situations that happened recently <br />
             (Example. I have lost my keys.)
           </li>
           <li>
@@ -178,7 +179,7 @@ export default function Tenses() {
         </h3>
         <ul>
           <li>
-            actions finished before another past action <br />
+            Actions finished before another past action <br />
             (Example. When I entered the room, Jassica had already finished
             studying.)
           </li>
@@ -188,9 +189,142 @@ export default function Tenses() {
             19)
           </li>
           <li>
-            Things that are impossible.
+            Things that are impossible. (wish)
             <br />
             (Example. I wish I had not done those silly things.)
+          </li>
+        </ul>
+        <h3>
+          Future Perfect Tense <br />
+          will + have + V3
+        </h3>
+        <ul typeof="dot">
+          <li>
+            How long a situation will happen in the future <br />
+            (Example. In coming February, I will have been single for 23 years.
+            <br />
+            When I come back home, my friend will have gone to oversea for 2
+            years.)
+          </li>
+          <li>
+            Action finished before a point in the future <br />
+            (Example. By 18:00, I will have finished my dinner.)
+          </li>
+        </ul>
+        <h3>Present Continuous Tense</h3>
+        <ul typeof="dot">
+          <li>
+            Currently unfinished actions <br />
+            (Example. I am doing my homework.)
+          </li>
+          <li>
+            Temporary situations (mostly used with at the moment, for a few
+            days, these days ) <br />
+            (Example. At the moment, I am typing my homeworks. <br /> I am
+            living in Yangon for a few more days.)
+          </li>
+          <li>
+            Temporary habits <br /> (Example. He is playing that song a lot
+            these days.)
+          </li>
+          <li>
+            Annoying habits(mostly used with always, constantly, forever) <br />
+            (Example. He is always getting late for work.)
+          </li>
+          <li>
+            Definite future plans(needs an exact time for the meaningful
+            sentence) <br />
+            (Example. I am leaving this town tomorrow.)
+          </li>
+        </ul>
+        <h3>
+          Past Continuous Tense <br />
+          was/were + Ving
+        </h3>
+        <ul typeof="dot">
+          <li>
+            Overlapping actions in the past (When V2(first action), was/were +
+            Ving(second action)) <br />
+            (Example. When I entered the classroom, the teacher was sitting in
+            front of the class.)
+          </li>
+          <li>
+            Story backgrounds <br /> (Example. At that day, the sun was shinning
+            brightly.)
+          </li>
+          <li>
+            Habits from the past <br /> (Example. When you are young, you were
+            always crying. <br /> In early 2020, I was walking a lot.)
+          </li>
+          <li>
+            Showing lenght of actions(all day, for (length of time))
+            <br />
+            (Example. It was raining for 3 hours.)
+          </li>
+          <li>
+            Showing exact time of the statement <br />
+            (Example. I was studying at 18:00 yesterday. )
+          </li>
+        </ul>
+        <h3>
+          Future Continouous Tense <br /> will + be + Ving
+        </h3>
+        <ul typeof="dot">
+          <li>
+            Overlapping actions in the future(When V1, will be + Ving) <br />
+            (Example. When mom comes back from the market, I will be studying.)
+          </li>
+          <li>
+            Future as a matter of course (Example. I will be a developer in 2
+            years.)
+          </li>
+        </ul>
+        <h3>
+          Present Perfect Continuous Tense <br />
+          has/have + been + Ving
+        </h3>
+        <ul typeof="dot">
+          <li>
+            Present result(action finished) <br /> (Example. I have been
+            sleeping so I didn't know what happened.)
+          </li>
+          <li>
+            Showing length of time (still unfinished) <br />
+            (Example. I have been studying for three hours.)
+          </li>
+          <li>
+            Temporary situations (still unfinished, use with recently) <br />
+            (Example. I have been working out a lot recently.)
+          </li>
+        </ul>
+        <h3>
+          Past Perfect Continuous Tense <br />
+          had + been + Ving
+        </h3>
+        <ul typeof="dot">
+          <li>
+            How long an action happened in the past <br />
+            (Example. Jonathan had been working at that company for 2 years when
+            Jassica started working there.)
+          </li>
+          <li>
+            Result at a time in the past <br /> (Example. My clothes were dirty,
+            I had been playing football.)
+          </li>
+        </ul>
+        <h3>
+          Future Perfect Continuous Tense <br />
+          will + have + been + Ving
+        </h3>
+        <ul typeof="dot">
+          <li>
+            How long to a point in the future <br />
+            (Example. I have been studying programming for 2 months on next
+            Sunday.)
+          </li>
+          <li>
+            Action finished before a point in the future <br />
+            (Example. When you arrive my home, I wil have been studying.)
           </li>
         </ul>
       </div>
